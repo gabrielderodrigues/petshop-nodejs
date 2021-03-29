0d0330f7ea46116b75e2b9e25e5dff9df72c0e31
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2021-03-29 11:45:38 -0300

    Configurando index.js para converter os dados para o JSON

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-var moment = require('moment');
-
+const moment = require('moment');
 const fs = require('fs');
+// Lê de forma sincrona o arquivo JSON através do modulo fs e converte em JS object
+const database = JSON.parse(fs.readFileSync('./database.json'));
+// Acessa o array de animas
+const animais = database.animais;
 
-const nomePetShop = "AvaPet";
+const nomePetShop = "AvaPet"
 
-// const animais = require('./dataBase.json')
-
-const animais = JSON.parse(fs.readFileSync('./database.json'));
-
-const WriteJson = (animais) => {
+// Atenção na hora de chamar essa funcao! O parâmetro passado deve ser db_pets para
+// garantir a sobrescrita correta do arquivo json
+const writeJson = (animais) => {
   // Converte o JS object atualizado em JSON e sobrescreve o db-pets.json de forma sincrona
   var _animais = JSON.stringify(animais);
-  fs.writeFileSync('database.json', _animais);
+  fs.writeFileSync('database.json', _animais, 'utf-8');
 }
 
-const listarPets = () => {
-  for (let i=0;i<animais.length;i++) {
-    console.log(animais[i].nome);
+const listarAnimais = () => {
+  for(let animal of animais) {
+    console.log(`${animal.nome}, ${animal.idade}`);
+    console.log(animal.vacinado ? 'O animal já está vacinado.' : 'O animal ainda não foi vacinado.')
   }
 }
 
-listarPets();
+listarAnimais();
 
-console.log()
-
-const vacinarPet = (animais) => {
-  if (!animais.vacinado) {
-    animais.vacinado = true;
-    console.log(`O ${animais.nome} acabou de ser vacinado!`);
+const vacinarAnimais = (animal) => {
+  if(!animal.vacinado) {
+    animal.vacinado === true;
+    console.log(`${animal.nome} acabou de ser vacinado.`);
   } else {
-    console.log(`O ${animais.nome} já está vacinado!`);
-  };
-};
-
-const campanhaVacina = (animais) => {
-  animaisVacinados = animais.filter(animais => animais.vacinado === false);
-  for(let animal of animais)
-      vacinarPet(animal);
-  console.log(`\nAnimais vacinados na campanha: ${animaisVacinados.length}.`);
-  WriteJson(animais);
+    console.log(`${animal.nome} foi vacinado anteriormente.`);
+  }
 }
 
-campanhaVacina();
+const campanhaVacina = (animais) => {
+  let animaisVacinados = animais.filter(animais => animais.vacinado === false);
+  for(let animal of animais)
+      vacinarAnimais(animal);
+  console.log(`\nAnimais vacinados na campanha: ${animaisVacinados.length}.`);
+  writeJson(database);
+}
 
-function inserirCliente(nome, tipo, raca, idade, peso, dono, vacinado) {
+campanhaVacina(animais);
+
+const inserirCliente = (nome, tipo, raca, idade, peso, dono, vacinado) => {
   newAnimal = {
     nome: nome,
     tipo: tipo,
@@ -54,12 +54,11 @@ function inserirCliente(nome, tipo, raca, idade, peso, dono, vacinado) {
     vacinado: vacinado,
     servicos: []
   }
-
   animais.push(newAnimal);
-  WriteJson(animais);
-}
+  writeJson(database);
+};
 
-inserirCliente('Luiz', 'jumento', 'cabrobro', 5, 100, 'Alice', true)
+inserirCliente('Luiz', 'jumento', 'cabrobro', 5, 100, 'Alice', true, []);
 
 console.log();
 console.log(animais);
@@ -81,29 +80,27 @@ darBanhoAnimal(animais[2]);
 darBanhoAnimal(animais[4]);
 
 const tosarAnimal = (animal) => {
-  if (!animal.servicos.includes('tosar')) {
-    animal.servicos.push('tosar');
+  if (!animal.servicos.includes('tosa')) {
+    animal.servicos.push('tosa');
     console.log(moment().format("L - LTS"));
     console.log(`${animal.nome} está tosado.`)
   } else {
     console.log(`${animal.nome} já foi tosado.`)
   }
 }
-
 console.log()
 console.log("== TOSAR ==")
 tosarAnimal(animais[1]);
 tosarAnimal(animais[4]);
 tosarAnimal(animais[3]);
 
+
 const apararUnhasAnimal = (animal) => {
-  if (!animal.servicos.includes('aparar unhas')) {
-    animal.servicos.includes('aparar unhas');
-    console.log(moment().format("L - LTS"));
-    console.log(`${animal.nome} está de unhas aparadas.`)
-  } else {
-    console.log(`${animal.nome} já teve as unhas aparadas.`)
-  }
+  animal.servicos.push({
+    servico: "unha",
+    data: moment().format("L - LTS")
+  });
+  console.log(`${animal.nome} está de unhas aparadas!`);
 }
 
 console.log()
@@ -112,9 +109,12 @@ apararUnhasAnimal(animais[1]);
 apararUnhasAnimal(animais[4]);
 apararUnhasAnimal(animais[3]);
 
-const atenderCliente = (animais, servico) => {
-  servico(animais);
-  WriteJson(animais);
-};
+const atenderCliente = (animal, servico) => {
+  servico(animal);
+  writeJson(database);
+}
 
-atenderCliente(animais[1], darBanhoAnimal);
+console.log()
+console.log("== ATENDER CLIENTE ==")
+atenderCliente(animais[0], apararUnhasAnimal);
+atenderCliente(animais[1], apararUnhasAnimal);
